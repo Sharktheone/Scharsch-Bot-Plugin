bd7013481ebefde8404ffd3bd8d92617e1edb745
Sharktheone <sharktheone@proton.me>
2023-04-29 14:30:41 +0200

    add events

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+extern crate jni;
+
+use jni::JNIEnv;
+use jni::objects::{JClass, JObject};
+
+
+#[no_mangle]
+pub unsafe extern "C" fn Java_de_scharschbot_plugin_Events_onInitialize(mut env: JNIEnv, class: JClass) {
+
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn Java_de_scharschbot_plugin_Events_onPlayerJoin(mut env: JNIEnv, class: JClass, event: JObject) {
+
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn Java_de_scharschbot_plugin_Events_onPlayerLeave(mut env: JNIEnv, class: JClass, event: JObject) {
+
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn Java_de_scharschbot_plugin_Events_onPlayerChat(mut env: JNIEnv, class: JClass, event: JObject) {
+
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn Java_de_scharschbot_plugin_Events_onPlayerDeath(mut env: JNIEnv, class: JClass, event: JObject) {
+
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn Java_de_scharschbot_plugin_Events_onPlayerAdvancement(mut env: JNIEnv, class: JClass, event: JObject) {
+
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn Java_de_scharschbot_plugin_Events_onShutdown(_env: JNIEnv, _class: JClass, _event: JObject) {
+    // TODO: Close websocket
 }
