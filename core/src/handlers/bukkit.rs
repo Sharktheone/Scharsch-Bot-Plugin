55bf5af91cef191c45a9c3b68c425273ffb2fe90
Sharktheone <sharktheone@proton.me>
2023-05-04 20:03:23 +0200

    remove logging of empty error

--- core/src/handlers/bukkit.rs
+++ core/src/handlers/bukkit.rs
@@ -2,11 +2,11 @@ use jni::objects::JClass;
 use scharschbot_core::jni_utils::get_env;
 use scharschbot_core::plugin::logger::error;
 
-pub(crate) fn get_bukkit() -> Result<JClass, ()> {
+pub(crate) fn get_bukkit() -> Result<JClass<'static>, ()> {
     let mut env = match get_env() {
         Ok(env) => env,
-        Err(e) => {
-            error(format!("Error getting env: {:?}", e));
+        Err(_) => {
+            error("Error getting env");
             return Err(());
         }
     };
