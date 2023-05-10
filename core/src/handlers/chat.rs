c8e33cb7af30cd0c7344d452ff2415cbe09a4491
Sharktheone <sharktheone@proton.me>
2023-05-10 17:08:08 +0200

    send admin message / with permission

--- core/src/handlers/chat.rs
+++ core/src/handlers/chat.rs
@@ -73,5 +73,75 @@ pub(crate) fn send_message(message: String, is_component: bool) -> Result<(), St
     };
 
     Ok(())
+}
 
+pub(crate) fn send_admin_message(message: String, is_component: bool, permission: Option<String>) -> Result<(), String> {
+    let mut env = match get_env() {
+        Ok(env) => env,
+        Err(_) => {
+            return Err("Error getting env".to_string());
+        }
+    };
+
+    let bukkit = match get_bukkit() {
+        Ok(bukkit) => bukkit,
+        Err(_) => {
+            return Err("Error getting bukkit".to_string());
+        }
+    };
+
+    let component = match parse_component(message, is_component) {
+        Ok(component) => component,
+        Err(_) => {
+            let msg = Message {
+                event: ERROR,
+                data: MessageData {
+                    players: None,
+                    player: None,
+                    uuid: None,
+                    reason: None,
+                    command: None,
+                    message: None,
+                    death_message: None,
+                    message_is_component: None,
+                    advancement: None,
+                    password: None,
+                    user: None,
+                    error: Some("Failed to parse component / text".to_string()),
+                    server: None,
+                }
+            };
+
+            match send(msg) {
+                Ok(_) => {},
+                Err(e) => {
+                    return Err(format!("Error sending error message: {}", e));
+                }
+            };
+            return Err("Error parsing component / text".to_string());
+        }
+    };
+
+    let broadcast_arg = JValue::Object(&component);
+    let permission_arg = match permission {
+        Some(permission) => JValue::Object(&parse_component(permission, false).unwrap()),
+        None => JValue::Object(&parse_component("scharschbot.admin".to_string(), false).unwrap()),
+    };
+
+    let _ = match env.call_static_method(&bukkit, "broadcast", "(Lnet/kyori/adventure/text/Component;Ljava/lang/String;)I", &[broadcast_arg, permission_arg]) {
+        Ok(players_received) => match players_received.i() {
+            Ok(players_received) => {
+                let players: i32 = players_received;
+                players
+            },
+            Err(e) => {
+                return Err(format!("Error getting broadcast return value: {}", e));
+            }
+        },
+        Err(e) => {
+            return Err(format!("Error calling broadcast: {}", e));
+        }
+    };
+
+    Ok(())
 }
